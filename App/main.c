bc71641af9dd6adc3c693558a4a88757221594f3
minhcore <dpnhatminh11@gmail.com>
2025-12-25 18:01:13 +0700

    new ide

--- App/main.c
+++ App/main.c
@@ -1,34 +1,21 @@
 #include <msp430.h>
 #include "bsp_gpio.h"
-#include "bsp_uart.h"
-#include "bsp_pwm.h"
-#include "bsp_system.h"
-#include "bsp_timer.h"
-#include "app_console.h"
-#include "app_servo.h"
-#include "app_controller.h"
+
 
 
 void main (void)
 {
 	WDTCTL = WDTPW + WDTHOLD;
 	BSP_System_InitClock_16MHZ();
-	App_Controller_Init();
+	BSP_GPIO_Init();
 	
 	
 	
         __bis_SR_register(GIE);
 	while (1)
         {
-			App_Console_Task();
-		  
-			if (BSP_10ms_Flag())
-			{
-				// auto delete Flag();
-				App_Controller_Task();
-				App_Send_Log_Data();
-				
-			}
+			__delay_cycles(16000000);
+			BSP_LED_Toggle();
 			
 			
           
