ef89bf7c4ca14ac7eba5de1c38d2ae55c262c79b
minhcore <dpnhatminh11@gmail.com>
2026-01-13 11:23:40 +0700

    test: verify CI failure on logic and style

--- src/main.c
+++ src/main.c
@@ -4,10 +4,11 @@
 
 int main(void)
 {
-    WDTCTL = WDTPW + WDTHOLD;
+                        WDTCTL = WDTPW + WDTHOLD;
     P1DIR = LED_PIN;
     P1OUT = LED_PIN;
-
+    int a[2];
+    a[10] = 50;
     while (1) {
         P1OUT ^= LED_PIN;
         __delay_cycles(500000);
