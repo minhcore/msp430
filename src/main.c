bd12d965d188a3228d41c79aafc16acc1c04d860
minhcore <dpnhatminh11@gmail.com>
2026-01-18 12:31:08 +0700

    fix(main.c): fix failure CI due to failed compilation

--- src/main.c
+++ src/main.c
@@ -1,12 +1,13 @@
 #include <msp430.h>
+
 #include "drivers/io.h"
 #include "drivers/mcu_init.h"
+#include "drivers/led.h"
+
 #include "common/assert_handler.h"
 #include "common/defines.h"
-#include "drivers/led.h"
 
 int main(void)
 {
-    test_blink_led();
     return 0;
 }
